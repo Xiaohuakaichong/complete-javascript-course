e38cd0f0fd3b775f491254273dc045b7034de2e2
HX-Desktop <zjsxhx622@gmail.com>
2022-09-28 02:13:52 +0800

    20220928

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -24,3 +24,20 @@ let age = 30;
 age = 31;
 
 const birthYear = 1995;
+const firstName = 'xia';
+const lastName = 'hua';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x++;
+x--;
+x += 30;
+x = x ** 2;
+console.log(x);
+
+//comparison operators
+let now = 2022;
+const ageWatson = now - 1995;
+const ageYinway = now - 1967;
+console.log(ageWatson > ageYinway); //会输出false 反之会输出true
+console.log(ageWatson > 18);
