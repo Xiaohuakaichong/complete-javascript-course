bd9d4081df6a7e6896add35f6cbbc26b02680fb0
HX-Laptop <zjsxhx622@gmail.com>
2022-09-29 18:35:47 +0800

    20220929

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,6 +20,7 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
 let age = 30;
 age = 31;
 
@@ -41,3 +42,35 @@ const ageWatson = now - 1995;
 const ageYinway = now - 1967;
 console.log(ageWatson > ageYinway); //会输出false 反之会输出true
 console.log(ageWatson > 18);
+*/
+
+// const now = 2022;
+// const ageWatson = now - 1995;
+// const ageYinway = now - 1967;
+
+// console.log(ageWatson > ageYinway);
+// console.log(25 - 10 - 5);
+
+// const averageAge = (ageWatson + ageYinway) / 2;
+// console.log(ageWatson, ageYinway);
+// console.log(averageAge);
+
+// console.log();
+
+const firstName = 'Xia';
+const job = 'interior designer';
+const birthYear = 1995;
+const year = 2022;
+
+const Watson = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job;
+
+console.log(Watson);
+
+const watsonNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(watsonNew);
+
+console.log(`just a simple string`);
+
+console.log(`String
+multiple
+lines`);
